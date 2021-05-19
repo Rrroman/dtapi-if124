8c1680a2f09c056e679845f3e8cae3264cd3ec21
Vlad Sevostianov <flypsoff@gmail.com>
2021-05-19 19:19:51 +0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ScheduleComponent } from './schedule/schedule.component';
+import { SubjectsVComponent } from './subjects-v/subjects-v.component';
 
 const routes: Routes = [
   { path: 'schedule', component: ScheduleComponent },
+  { path: 'subjectsV', component: SubjectsVComponent },
   { path: '**', redirectTo: '/' },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MatButtonModule } from '@angular/material/button';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormsModule } from '@angular/forms';
-import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatTableModule } from '@angular/material/table';
+import { MatSortModule } from '@angular/material/sort';
+
 import { AppComponent } from './app.component';
+
+import { AppRoutingModule } from './app-routing.module';
+
 import { ScheduleComponent } from './schedule/schedule.component';
+import { SubjectsVComponent } from './subjects-v/subjects-v.component';
 
 @NgModule({
-  declarations: [AppComponent, ScheduleComponent],
+  declarations: [AppComponent, ScheduleComponent, SubjectsVComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -17,6 +25,9 @@ import { ScheduleComponent } from './schedule/schedule.component';
     BrowserAnimationsModule,
     FormsModule,
     MatFormFieldModule,
+    MatTableModule,
+    HttpClientModule,
+    MatSortModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
