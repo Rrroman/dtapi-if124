5acb1c2885ead4408dce722379f996f3e472e82b
Vlad Sevostianov <flypsoff@gmail.com>
2021-05-20 21:24:22 +0300

    added interceptor, component and one material module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,14 +7,16 @@ import { MatInputModule } from '@angular/material/input';
 import { MatTableModule } from '@angular/material/table';
 import { MatSortModule } from '@angular/material/sort';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { MatDialogModule } from '@angular/material/dialog';
 import { ScheduleComponent } from './schedule/schedule.component';
 import { SubjectsRComponent } from './subjects-r/subjects-r.component';
 import { SubjectsVComponent } from './subjects-v/subjects-v.component';
 import { LoginComponent } from './login/login.component';
+import { DialogChangeComponent } from './subjects-v/dialog-change/dialog-change.component';
+import { TokenInterceptor } from 'src/interceptors/token.interceptor';
 
 @NgModule({
   declarations: [
@@ -23,6 +25,7 @@ import { LoginComponent } from './login/login.component';
     LoginComponent,
     SubjectsRComponent,
     SubjectsVComponent,
+    DialogChangeComponent,
   ],
 
   imports: [
@@ -37,8 +40,15 @@ import { LoginComponent } from './login/login.component';
     MatSortModule,
     ReactiveFormsModule,
     HttpClientModule,
+    MatDialogModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      multi: true,
+      useClass: TokenInterceptor,
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
